e47882f042ac1e5531625cbfec69e7228f1cb9b8
wbw121124 <wbw121124@163.com>
2026-09-07 15:41:18 +0800

    fix: 修复 CI 配置，更新 README 安装说明

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.js', '**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
